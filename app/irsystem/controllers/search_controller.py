5b5458f08b7336c2c10483814a427e46a82a28af
Keyang Yu <keyangyu@KeyangdeMacBook-Pro.local>
2021-04-16 16:34:16 -0400

    search controller bug resolve

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -11,13 +11,12 @@ net_id = "Jiejun Zhang, jz2252; Keyang Yu, ky442; Shuyi Gu, sg2474; Wei Cheng, w
 def search():
 	query = request.args.get('search')
 	if not query:
-		data = []
+		return_data = []
 		output_message = ''
 	else:
 		output_message = "Your search: " + query
 		similarity_score_list = compute_cosine_similarity(query,leetcode_data)
 		similarity_score_list = sorted(enumerate(similarity_score_list), key=lambda x:x[1][1], reverse=True)
-		# return_data = similarity_score_list[:5]
 		return_data = [x[1] for x in similarity_score_list[:5]]
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=return_data)
 
