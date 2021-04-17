eaaa7c55c93143e5dc8ebc009dbb43397866ac3c
sutan1206 <sutan1206@gmail.com>
2021-04-17 17:45:52 -0400

    Updated frontend

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -12,17 +12,15 @@ NUM_TOP_QUESTIONS = 5
 NUM_TOP_HINTS = 5
 
 def getSortedTopTags(topQuestions):
-  MIN_Q_SCORE = 0.1
   totalRelevance = 0
   tagToScore = defaultdict(int)
 
   for title, _, score in topQuestions:
-    if score >= MIN_Q_SCORE:
-      totalRelevance += score
+    totalRelevance += score
 
-      for tag in titleToTags[title]:
-        if tag not in NON_HINT_TAGS:
-          tagToScore[tag] += score
+    for tag in titleToTags[title]:
+      if tag not in NON_HINT_TAGS:
+        tagToScore[tag] += score
 
   return sorted(((t, s / totalRelevance) for t, s in tagToScore.items()), key=lambda x: x[1], reverse=True)[:NUM_TOP_HINTS]
 
@@ -31,13 +29,11 @@ def search():
   query = request.args.get('search')
 
   if not query:
-    output_message = ''
+    query = ''
     topQuestions = []
     topTags = []
 
   else:
-    output_message = "Your search: " + query
-
     # Type: [(title, score)], not sorted by score.
     similarity_score_list = compute_cosine_similarity(query, leetcode_data)
     similarity_score_list.sort(key=lambda x: x[1], reverse=True)
@@ -52,4 +48,4 @@ def search():
     print(topHints)
     print()
 
-  return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=topQuestions)
+  return render_template('search.html', name=project_name, netid=net_id, query=query, topQuestions=topQuestions, topHints=topHints)
